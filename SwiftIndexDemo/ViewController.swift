7d1b13869044b47a940a3d8d7ded65ec32bd828a
LicaGao <LicaGao@users.noreply.github.com>
2017-11-20 16:52:51 +0800

    11.20练习

---
+++ SwiftIndexDemo/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  SwiftIndexDemo
+//
+//  Created by 高鑫 on 2017/11/20.
+//  Copyright © 2017年 高鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let sections = Model.sections
+    let indexes = Model.indexes
+    let result = Model.result
+    var searchResult : [String] = []
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return self.sections
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        var index : Int = 0
+        for i in sections {
+            if i == title {
+                return index
+            }
+            index += 1
+        }
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return indexes.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sections[section]
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CityNameTableViewCell
+        let section = sections[indexPath.section]
+        let index = indexes[indexPath.row]
+        cell.selectionStyle = .none
+        cell.indexLabel.text = section + " - " + index
+        return cell
+    }
+    
+}
+
+class CityNameTableViewCell: UITableViewCell {
+    @IBOutlet weak var indexLabel: UILabel!
+    
+}
